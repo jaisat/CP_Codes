afd30cb6b2febcb0afc7c400ed0367f85a39e41c
jaisat <jaiswalsatyam211@gmail.com>
2021-09-11 12:12:11 +0530

    codes added

--- RangeQueries/prefixSum.cpp
+++ RangeQueries/prefixSum.cpp
@@ -26,53 +26,78 @@ using namespace std;
 mt19937                 rng(chrono::steady_clock::now().time_since_epoch().count());
  
 typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+ 
 
-int n;
-vi arr,seg,present;
+vi arr,seg,lazy,pre;
+int n,q;
 
-void buildTree(int s,int e,int idx){
-	if(s == e){
-		seg[idx] = 1;
+void rangeUpdate(int s,int e,int qs,int qe,int change,int idx){
+
+	if(lazy[idx] != 0){
+		seg[idx] += (e - s + 1) * lazy[idx];
+		if(s != e){
+			lazy[2*idx]   += lazy[idx];
+			lazy[2*idx+1] += lazy[idx];
+		}
+		lazy[idx] = 0;
+	}
+
+	if(s > qe || e < qs)
+		return;
+
+	if(s >= qs and e <= qe){
+		seg[idx] += (e - s + 1) * change;
+		if(s != e){
+			lazy[2*idx]   += change;
+			lazy[2*idx+1] += change;
+		}
 		return;
 	}
 
 	int mid = (s + e) / 2;
-	buildTree(s,mid,2*idx);
-	buildTree(mid+1,e,2*idx+1);
+	rangeUpdate(s,mid,qs,qe,change,2*idx);
+	rangeUpdate(mid+1,e,qs,qe,change,2*idx+1);
 	seg[idx] = seg[2*idx] + seg[2*idx+1];
 	return;
 }
 
 int query(int s,int e,int qs,int qe,int idx){
-	if(s > qe || e < qs){
-		return 0;
+	if(lazy[idx] != 0){
+		seg[idx] += (e - s + 1) * lazy[idx];
+		if(s != e){
+			lazy[2*idx]   += lazy[idx];
+			lazy[2*idx+1] += lazy[idx];
+		}
+		lazy[idx] = 0;
 	}
+
+	if(s > qe || e < qs)
+		return 0;
 
 	if(s >= qs and e <= qe){
 		return seg[idx];
 	}
-
 	int mid = (s + e) / 2;
 	int L = query(s,mid,qs,qe,2*idx);
 	int R = query(mid+1,e,qs,qe,2*idx+1);
 	return L + R;
 }
 
-void update(int s,int e,int i,int idx){
-	if(i < s || i > e){
-		return;
-	}
 
+void buildTree(int s,int e,int idx){
 	if(s == e){
-		seg[idx] = 0;
+		seg[idx] = pre[s];
 		return;
 	}
+	
 	int mid = (s + e) / 2;
-	update(s,mid,i,2*idx);
-	update(mid+1,e,i,2*idx+1);
+
+	buildTree(s,mid,2*idx);
+	buildTree(mid+1,e,2*idx+1);
 	seg[idx] = seg[2*idx] + seg[2*idx+1];
 	return;
 }
+
 
 void c_p_c()
 {
@@ -81,48 +106,29 @@ void c_p_c()
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
-    cin >> n;
+    cin >> n >> q;
     arr.resize(n);
-    seg.resize(4*n+1);
-    present.resize(n,1);
+    pre.resize(n);
+    arr.resize(4*n+1);
+    for(int i=0;i<n;i++) cin >> arr[i];
 
-    for(int i=0;i<n;i++){
-    	cin >> arr[i];
+    pre[0] = arr[0];
+    for(int i=1;i<n;i++){
+    	pre[i] = pre[i-1] + arr[i];
     }
-    buildTree(0,n-1,1);
-   
-    for(int i=0;i<n;i++){
-    	int id;
-    	cin >> id;
 
-    	int s = 0;
-    	int e = n-1;
-    	int ans_id = -1;
+    while(q--){
+    	int type;
+    	cin >> type;
+    	if(type == 1){
+    		int i,u;
+    		cin >> i >> u;
 
-    	while(s <= e){
-    		int mid = (s + e) / 2;
-    		int sub = query(0,n-1,0,mid,1);
-    		if(sub > id){
-    			e = mid - 1;
-    		}else if(sub < id){
-    			s = mid + 1;
-    		}else{
-    			if(present[mid]){
-    				ans_id = mid;
-    				break;
-    			}
-    			else{
-    				e = mid - 1;
-    			}
-    		}
+    	}else{
+    		int a,b;
+    		cin >> a >> b;
+    		cout<<query(0,n-1,a-1,b-1,1)<<"\n";
     	}
-
-    	present[ans_id] = 0;
-    	update(0,n-1,ans_id,1);
-    	
-    	//cout<<ans_id<<" ";
- 
-    	cout<<arr[ans_id]<<" ";
     }
 }
  
